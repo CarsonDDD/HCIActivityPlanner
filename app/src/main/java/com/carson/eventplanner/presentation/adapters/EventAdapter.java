a90b7bffa3c698d4b5b9121127a4bba094e54269
Carson <carson-dickinson@hotmail.com>
2023-03-12 00:51:12 -0600

    Small refactor

---
+++ app/src/main/java/com/carson/eventplanner/presentation/adapters/EventAdapter.java
@@ -0,0 +1,75 @@
+package com.carson.eventplanner.presentation.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.carson.eventplanner.R;
+import com.carson.eventplanner.objects.Event;
+import com.carson.eventplanner.objects.EventCategory;
+
+import java.util.List;
+
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHolder> {
+
+    private final List<Event> eventList;
+    //private final OnItemClickListener listener;
+
+    /*public interface OnItemClickListener {
+        void onItemClick(Event eventCategory);
+    }*/
+
+    public EventAdapter(List<Event> eventList/*, OnItemClickListener listener*/) {
+        this.eventList = eventList;
+        //this.listener = listener;
+    }
+
+
+    @NonNull
+    @Override
+    public EventAdapter.EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_event, parent, false);
+        return new EventAdapter.EventViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventAdapter.EventViewHolder holder, int position) {
+        holder.bind(eventList.get(position)/*, listener*/);
+    }
+
+    @Override
+    public int getItemCount() {
+        return eventList.size();
+    }
+
+    public static class EventViewHolder extends RecyclerView.ViewHolder {
+        private final TextView title;
+        private final TextView date;
+        //private final ImageView image;
+
+        public EventViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.tv_event_title);
+            date = itemView.findViewById(R.id.tv_event_date);
+
+            //image = itemView.findViewById(R.id.iv_event);
+        }
+
+        public void bind(Event event){
+            title.setText(event.getTitle());
+            date.setText(event.getDate());
+
+            /*itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(eventCategory);
+                }
+            });*/
+        }
+    }
+}
