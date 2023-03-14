7d1e9ecf4fed707e26b279e1b7b9e43d1bdf4b5b
carson-dickinson <carson-dickinson@hotmail.com>
2023-03-13 23:29:47 -0500

    Covert to fragment

---
+++ app/src/main/java/com/carson/eventplanner/DiscoveryFragment.java
@@ -0,0 +1,96 @@
+package com.carson.eventplanner;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.carson.eventplanner.objects.Event;
+import com.carson.eventplanner.objects.EventCategory;
+import com.carson.eventplanner.presentation.adapters.DrawerMenuAdapter;
+import com.carson.eventplanner.presentation.adapters.EventAdapter;
+import com.carson.eventplanner.presentation.adapters.EventCategoryAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiscoveryFragment extends Fragment {
+
+    // event views
+    private RecyclerView rvCategories, rvPopular, rvRecommended, rvUpcoming;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_discovery, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+
+        // Set up the event categories
+        rvCategories = view.findViewById(R.id.rv_event_categories);
+        List<EventCategory> eventCategories = new ArrayList<>();
+        eventCategories.add(new EventCategory("Category 1"));
+        eventCategories.add(new EventCategory("Category 2"));
+        eventCategories.add(new EventCategory("Category 3"));
+        EventCategoryAdapter eventCategoryAdapter = new EventCategoryAdapter(eventCategories);
+        rvCategories.setAdapter(eventCategoryAdapter);
+
+        // Popular events
+        rvPopular = view.findViewById(R.id.rv_popular_events);
+        //rvPopular.setLayoutManager(new LinearLayoutManager(this));
+        List<Event> popEventList = new ArrayList<>();
+        popEventList.add(new Event("golf with friends"));
+        popEventList.add(new Event("music in the park"));
+        popEventList.add(new Event("game night"));
+        EventAdapter popularEventsAdapter = new EventAdapter(popEventList);
+        rvPopular.setAdapter(popularEventsAdapter);
+
+        // Recommended Events
+        rvRecommended = view.findViewById(R.id.rv_recommended_events);
+        //rvRecommended.setLayoutManager(new LinearLayoutManager(this));
+        List<Event> recommendedList = new ArrayList<>();
+        recommendedList.add(new Event("shuffle board"));
+        recommendedList.add(new Event("bird watching"));
+        recommendedList.add(new Event("Complaining"));
+        EventAdapter recommendedEventsAdapter = new EventAdapter(recommendedList);
+        rvRecommended.setAdapter(recommendedEventsAdapter);
+
+        // Upcoming Events
+        rvUpcoming = view.findViewById(R.id.rv_upcoming_events);
+        //rvUpcoming.setLayoutManager(new LinearLayoutManager(this));
+        List<Event> upcomingList = new ArrayList<>();
+        upcomingList.add(new Event("golf with friends"));
+        upcomingList.add(new Event("music in the park"));
+        upcomingList.add(new Event("game night"));
+        EventAdapter upcomingEventsAdapter = new EventAdapter(upcomingList);
+        rvUpcoming.setAdapter(upcomingEventsAdapter);
+
+    }
+
+    // Event handlers for each card click
+    final private EventCategoryAdapter.OnItemClickListener categoryClick = new EventCategoryAdapter.OnItemClickListener() {
+        @Override
+        public void onItemClick(EventCategory eventCategory) {
+
+        }
+    };
+}
--- app/src/main/java/com/carson/eventplanner/presentation/MainActivity.java
+++ app/src/main/java/com/carson/eventplanner/presentation/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.SearchView;
 
+import com.carson.eventplanner.DiscoveryFragment;
 import com.carson.eventplanner.R;
 import com.carson.eventplanner.objects.Event;
 import com.carson.eventplanner.objects.EventCategory;
@@ -30,9 +32,6 @@ public class MainActivity extends AppCompatActivity {
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private Toolbar toolbar;
 
-    // event views
-    private RecyclerView rvCategories, rvPopular, rvRecommended, rvUpcoming;
-
     private RecyclerView rvDrawerMenu;
 
     @Override
@@ -40,8 +39,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new DiscoveryFragment());
+        fragmentTransaction.commit();
+
+
         // Set up the toolbar
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         // Add menu to toolbar
@@ -60,45 +64,6 @@ public class MainActivity extends AppCompatActivity {
         DrawerMenuAdapter drawerMenuAdapter = new DrawerMenuAdapter(drawerItems, menuClick);
         rvDrawerMenu.setAdapter(drawerMenuAdapter);
 
-        // Set up the event categories
-        rvCategories = findViewById(R.id.rv_event_categories);
-        //rvEventCategories.setLayoutManager(new LinearLayoutManager(this));
-        List<EventCategory> eventCategories = new ArrayList<>();
-        eventCategories.add(new EventCategory("Category 1"));
-        eventCategories.add(new EventCategory("Category 2"));
-        eventCategories.add(new EventCategory("Category 3"));
-        EventCategoryAdapter eventCategoryAdapter = new EventCategoryAdapter(eventCategories);
-        rvCategories.setAdapter(eventCategoryAdapter);
-
-        // Popular events
-        rvPopular = findViewById(R.id.rv_popular_events);
-        //rvPopular.setLayoutManager(new LinearLayoutManager(this));
-        List<Event> popEventList = new ArrayList<>();
-        popEventList.add(new Event("golf with friends"));
-        popEventList.add(new Event("music in the park"));
-        popEventList.add(new Event("game night"));
-        EventAdapter popularEventsAdapter = new EventAdapter(popEventList);
-        rvPopular.setAdapter(popularEventsAdapter);
-
-        // Recommended Events
-        rvRecommended = findViewById(R.id.rv_recommended_events);
-        //rvRecommended.setLayoutManager(new LinearLayoutManager(this));
-        List<Event> recommendedList = new ArrayList<>();
-        recommendedList.add(new Event("shuffle board"));
-        recommendedList.add(new Event("bird watching"));
-        recommendedList.add(new Event("Complaining"));
-        EventAdapter recommendedEventsAdapter = new EventAdapter(recommendedList);
-        rvRecommended.setAdapter(recommendedEventsAdapter);
-
-        // Upcoming Events
-        rvUpcoming = findViewById(R.id.rv_upcoming_events);
-        //rvUpcoming.setLayoutManager(new LinearLayoutManager(this));
-        List<Event> upcomingList = new ArrayList<>();
-        upcomingList.add(new Event("golf with friends"));
-        upcomingList.add(new Event("music in the park"));
-        upcomingList.add(new Event("game night"));
-        EventAdapter upcomingEventsAdapter = new EventAdapter(upcomingList);
-        rvUpcoming.setAdapter(upcomingEventsAdapter);
     }
 
     @Override
@@ -124,14 +89,6 @@ public class MainActivity extends AppCompatActivity {
         });*/
         return true;
     }
-
-    // Event handlers for each card click
-    final private EventCategoryAdapter.OnItemClickListener categoryClick = new EventCategoryAdapter.OnItemClickListener() {
-        @Override
-        public void onItemClick(EventCategory eventCategory) {
-
-        }
-    };
 
     final private DrawerMenuAdapter.OnItemClickListener menuClick = new DrawerMenuAdapter.OnItemClickListener() {
         @Override
