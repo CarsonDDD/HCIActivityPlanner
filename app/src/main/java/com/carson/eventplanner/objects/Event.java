86a7288698a92de392a246975b6c798dc2554681
Carson <carson-dickinson@hotmail.com>
2023-03-15 19:20:59 -0500

    flesh out DSO

--- app/src/main/java/com/carson/eventplanner/objects/Event.java
+++ app/src/main/java/com/carson/eventplanner/objects/Event.java
@@ -2,10 +2,26 @@ package com.carson.eventplanner.objects;
 
 public class Event {
     private String title;
-    //private String date;
+    private String date;
+    private String time;
+    private String location;
+    private String description;
+    private boolean isPublic;
+
+    // Image later
+
 
     public Event(String title) {
+        this(title,"TBA","TBA","TBA","An Event!", true);
+    }
+
+    public Event(String title, String date, String time, String location, String description, boolean isPublic){
         this.title = title;
+        this.date = date;
+        this.time = time;
+        this.location = location;
+        this.description = description;
+        this.isPublic = isPublic;
     }
 
     public String getTitle() {
@@ -13,11 +29,23 @@ public class Event {
     }
 
     public String getDate() {
-        return "April 1, 2020";
+        return date;
     }
 
-    public String getTime(){
-        return "5pm";
+    public String getTime() {
+        return time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
     }
 
     /*public int getImageResource() {
--- app/src/main/java/com/carson/eventplanner/objects/User.java
+++ app/src/main/java/com/carson/eventplanner/objects/User.java
@@ -1,17 +1,38 @@
 package com.carson.eventplanner.objects;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private String userName;
     //private ImageView pfp;
 
+    private List<User> friends;
+    private List<Event> createdEvents;
+    private List<Event> joinedEvents;
+
     public User(String userName) {
         this.userName = userName;
+        friends = new ArrayList<User>();
+        createdEvents = new ArrayList<Event>();
+        joinedEvents = new ArrayList<Event>();
     }
 
     public String getUserName() {
         return userName;
     }
 
+    public void addFriend(User friend){
+        friends.add(friend);
+    }
+
+    public void createEvent(Event event){
+        createdEvents.add(event);
+    }
+
+    public void joinEvent(Event event){
+        joinedEvents.add(event);
+    }
     /*public int getProfileImage() {
         return 0;
     }*/
